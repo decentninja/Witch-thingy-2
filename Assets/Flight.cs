706f901ca9288dc50031386fd3109de648f4259b
sootn <encrypt.wave@gmail.com>
2015-06-16 01:46:16 +0200

    New camera...

--- Assets/Flight.cs
+++ Assets/Flight.cs
@@ -3,20 +3,18 @@ using System.Collections;
 
 public class Flight : MonoBehaviour {
 	public float forward = 1,
-			   	 roll = 1,
 				 lift = 1,
-				 drag = 0.1f;
+				 drag = 0.1f,
+				 turnspeed = 0.1f;
 
-	float velocity = 0;
+	public Transform camera;
+	public Transform model;
 
 	void Update() {
-		if(Input.GetButton("Jump"))
-			transform.Rotate(lift, 0, 0, Space.Self);
-		if(Input.GetButton("Fire3"))
-			transform.Rotate(-lift, 0, 0, Space.Self);
-		velocity += Input.GetAxis("Vertical") * forward;
-		velocity *= drag;
-		transform.Rotate(0, 0, Input.GetAxis("Horizontal") * roll, Space.Self);
-		transform.Translate(0, 0, -velocity, Space.Self);
+		var old = model.rotation;
+		model.LookAt(model.position - camera.forward * 1000000);
+		model.rotation = Quaternion.Lerp(old, model.rotation, turnspeed);
+		float velocity = Input.GetAxis("Vertical") * forward;
+		transform.Translate(camera.forward * velocity);
 	}
 }
---
+++ Assets/lookscr.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lookscr : MonoBehaviour {
+     public float sensitivityX = 15F;
+     public float sensitivityY = 15F;
+
+     public float minimumX = -360F;
+     public float maximumX = 360F;
+
+     public float minimumY = -60F;
+     public float maximumY = 60F;
+
+     float rotationX = 0F;
+     float rotationY = 0F;
+
+     Quaternion originalRotation;
+
+     void Update()
+     {
+        rotationX += Input.GetAxis("Mouse X") * sensitivityX;
+        rotationY += Input.GetAxis("Mouse Y") * -sensitivityY;
+        rotationX = ClampAngle(rotationX, minimumX, maximumX);
+        rotationY = ClampAngle(rotationY, minimumY, maximumY);
+        Quaternion xQuaternion = Quaternion.AngleAxis(rotationX, Vector3.up);
+        Quaternion yQuaternion = Quaternion.AngleAxis(rotationY, -Vector3.right);
+        transform.localRotation = originalRotation * xQuaternion * yQuaternion;
+     }
+
+     void Start()
+     {
+         originalRotation = transform.localRotation;
+         Cursor.visible = false;
+     }
+
+     public static float ClampAngle(float angle, float min, float max)
+     {
+         if (angle < -360F)
+             angle += 360F;
+         if (angle > 360F)
+             angle -= 360F;
+         return Mathf.Clamp(angle, min, max);
+     }
+}
